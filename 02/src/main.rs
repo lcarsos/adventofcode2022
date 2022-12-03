451592998fe6fabe2d295cf58929f788e9a3f391
Ezekiel Chopper <lcarsos@lcarsos.com>
2022-12-03 10:58:26 -0700

    and then computing part two in parallel

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -31,13 +31,32 @@
 //   Z | 3 | 9 | 6
 //
 // So how much do I over-engineer the solution if it's that easy?
+//
+// part 2
+// X = Need lose
+// Y = Need draw
+// Z = Need win
+//
+// A, Rock,     1pt
+// B, Paper,    2pt
+// C, Scissors, 3pt
+//
+// A X => A C => 0 + 3 => 3
+// A Y => A A => 3 + 1 => 4
+// A Z => A B => 6 + 2 => 8
+// B X => B A => 0 + 1 => 1
+// B Y => B B => 3 + 2 => 5
+// B Z => B C => 6 + 3 => 9
+// C X => C B => 0 + 2 => 2
+// C Y => C C => 3 + 3 => 6
+// C Z => C A => 6 + 1 => 7
 
 use std::collections::HashMap;
 //use std::io::prelude::*;
 use std::io::{stdin, BufRead};
 
 fn main() {
-    let codex = HashMap::from([
+    let pt_one_codex = HashMap::from([
         ("A X", 4),
         ("A Y", 8),
         ("A Z", 3),
@@ -48,12 +67,25 @@ fn main() {
         ("C Y", 2),
         ("C Z", 6),
     ]);
+    let pt_two_codex = HashMap::from([
+        ("A X", 3),
+        ("A Y", 4),
+        ("A Z", 8),
+        ("B X", 1),
+        ("B Y", 5),
+        ("B Z", 9),
+        ("C X", 2),
+        ("C Y", 6),
+        ("C Z", 7),
+    ]);
 
-    let mut sum = 0;
+    let mut one_sum = 0;
+    let mut two_sum = 0;
     for line in stdin().lock().lines() {
-        //let index = line.unwrap();
-        sum += codex.get(&*line.unwrap()).unwrap();
+        let index = line.unwrap();
+        one_sum += pt_one_codex.get(&*index).unwrap();
+        two_sum += pt_two_codex.get(&*index).unwrap();
         //println!("{}", codex.get(&*line.unwrap()).unwrap());
     }
-    println!("{sum}")
+    println!("{one_sum} {two_sum}")
 }
