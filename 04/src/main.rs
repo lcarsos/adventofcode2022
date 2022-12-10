2c340ea54797adbb7a07acfc4cada25d4ac60045
lcarsos <lcarsos@lcarsos.com>
2022-12-09 18:51:13 -0700

    oh I was done. Still want to go back and use proper comparisons

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -8,22 +8,22 @@ struct Range {
 }
 
 impl Range {
-    fn contains(&self, &other: &Range) -> bool {
+    fn contains(&self, other: &Range) -> bool {
         return self.left <= other.left && self.right >= other.right;
     }
 }
 
 fn main() {
+    let mut count = 0;
     let re = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)").unwrap();
     for line in stdin().lock().lines() {
         let st = line.unwrap();
         let cap = re.captures(&st).unwrap();
         let range_a = Range { left: cap[1].parse().unwrap(), right: cap[2].parse().unwrap()};
         let range_b = Range { left: cap[3].parse().unwrap(), right: cap[4].parse().unwrap()};
-        let answer = range_a.contains(&range_b);
-
-        println!("a contains b: {}", answer);
-        println!("{} {}", range_a.left, range_a.right);
-        break;
+        if (range_a.contains(&range_b) || range_b.contains(&range_a)) {
+            count += 1;
+        }
     }
+    println!("count: {}", count);
 }
