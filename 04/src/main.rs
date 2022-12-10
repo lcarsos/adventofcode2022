153b2731836e1a0fa6876bf071533353ece529f5
Ezekiel Chopper <ezekiel.chopper@workday.com>
2022-12-09 17:28:29 -0700

    I'm too stupid to figure out if I can do this with cmp

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -7,14 +7,22 @@ struct Range {
     right: u64
 }
 
+impl Range {
+    fn contains(&self, &other: &Range) -> bool {
+        return self.left <= other.left && self.right >= other.right;
+    }
+}
+
 fn main() {
     let re = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)").unwrap();
     for line in stdin().lock().lines() {
         let st = line.unwrap();
         let cap = re.captures(&st).unwrap();
         let range_a = Range { left: cap[1].parse().unwrap(), right: cap[2].parse().unwrap()};
-        //let nibbleA: u64 = cap[1].parse().unwrap();
-        //let nibbleB: u64 = cap[2].parse().unwrap();
+        let range_b = Range { left: cap[3].parse().unwrap(), right: cap[4].parse().unwrap()};
+        let answer = range_a.contains(&range_b);
+
+        println!("a contains b: {}", answer);
         println!("{} {}", range_a.left, range_a.right);
         break;
     }
