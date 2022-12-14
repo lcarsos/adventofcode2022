20444aa9bc1765f69eef79e908a31e5cc436ff85
Ezekiel Chopper <lcarsos@lcarsos.com>
2022-12-14 16:05:31 -0700

    figuring out how to pass generics instead of specifics

--- 05/src/main.rs
+++ 05/src/main.rs
@@ -1,16 +1,16 @@
-use std::io::{stdin, BufRead, BufReader};
+use std::io::{stdin, Read, BufRead, BufReader};
 use regex::Regex;
 
-fn parse_crates(stream: BufReader<R>) -> Vec<Vec<char>> {
+fn parse_crates(stream: &mut dyn Read) -> Vec<Vec<char>> {
     let mut buffer = [0; 5];
 
     let mut handle = stream.take(3);
-    handle.read(&mut buffer);
+    handle.read(&mut buffer).unwrap();
     return vec!(vec!('a','b'));
 }
 
 fn main() {
-    let mut foo = parse_crates(stdin().lock());
+    let mut foo = parse_crates(&mut stdin().lock());
     //for line in stdin().lock().lines() {
     //}
     println!("Hello, world!");
