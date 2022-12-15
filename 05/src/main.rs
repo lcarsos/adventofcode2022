69de54f35fa8323f7fb1e0250eab9d0777c341fd
Ezekiel Chopper <ezekiel.chopper@workday.com>
2022-12-14 17:00:23 -0700

    oh cool the return model of the language I learned a few years ago changed

--- 05/src/main.rs
+++ 05/src/main.rs
@@ -1,16 +1,35 @@
 use std::io::{stdin, Read, BufRead, BufReader};
 use regex::Regex;
 
-fn parse_crates(stream: &mut dyn Read) -> Vec<Vec<char>> {
-    let mut buffer = [0; 5];
+fn parse_crates<R>(stream: &mut R) -> Option<Vec<Vec<char>>>
+where
+    R: BufRead,
+{
+    let mut buffer = vec![0; 5];
 
     let mut handle = stream.take(3);
     handle.read(&mut buffer).unwrap();
+    if buffer[1] == 32 {
+        println!("found space");
+    }
+    handle = stream.take(1);
+    handle.read(&mut buffer).unwrap();
+    println!("{}", buffer[0]);
+    match buffer[0] {
+        b'\n' => println!("newline"),
+        b' ' => println!("space"),
+        _ => panic!("parse could not match expected character"),
+    }
+    if buffer[0] == 10 {
+        
+    }
+
+
     return vec!(vec!('a','b'));
 }
 
 fn main() {
-    let mut foo = parse_crates(&mut stdin().lock());
+    let mut foo = parse_crates(&mut stdin().lock())?;
     //for line in stdin().lock().lines() {
     //}
     println!("Hello, world!");
