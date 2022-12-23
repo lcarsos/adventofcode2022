ba4155b9806b05b4359ba84aa5996047bc92bb18
lcarsos <lcarsos@lcarsos.com>
2022-12-23 12:27:43 -0700

    finished parsing the depot

--- 05/src/main.rs
+++ 05/src/main.rs
@@ -6,51 +6,82 @@ where
     R: BufRead,
 {
     let mut buffer = vec![0; 5];
-    let mut scanlines: Vec<Vec<Option<u8>>> = Vec::new();
+    let mut scanlines: Vec<Vec<Option<char>>> = Vec::new();
     scanlines.push(Vec::new());
-    let mut stack = 0;
-    let mut row = 0;
     let digit_re = Regex::new(r"^[0-9]$").unwrap();
 
     let mut handle;
+    //println!("scanlines {} last {}", scanlines.len(), scanlines.last().unwrap().len());
     loop {
         handle = stream.take(3);
         handle.read(&mut buffer).unwrap();
         if buffer[0] == b' ' &&
             digit_re.is_match(&(buffer[1] as char).to_string())
         {
-            println!("breaking");
+            //println!("breaking");
+            scanlines.pop();
             break;
         }
+
         match buffer[1] {
             b' ' => {
-                scanlines[row].push(None);
-                println!("found space");
+                scanlines.last_mut().unwrap().push(None);
+                //println!("found space");
             },
             x => {
-                println!("pushing {}", x as char);
-                scanlines[row].push(Some(x));
+                //println!("pushing {}", x as char);
+                scanlines.last_mut().unwrap().push(Some(x as char));
             }
         }
+        //println!("scanlines {} last {}", scanlines.len(), scanlines.last().unwrap().len());
         handle = stream.take(1);
         handle.read(&mut buffer).unwrap();
-        println!("{}", buffer[0]);
+        //println!("break type:{}", buffer[0]);
         match buffer[0] {
             b'\n' => {
                 scanlines.push(Vec::new());
-                println!("newline");
+                //println!("newline");
             },
-            b' ' => println!("space"),
+            b' ' => {
+                //println!("space")
+            },
             _ => panic!("parse could not match expected character"),
         }
     }
 
+    //println!("\nstack\n");
+    //for row in scanlines.iter().rev() {
+    //    let crate_char: char = match row[1] {
+    //        Some(x) => x,
+    //        None => ' ',
+    //    };
+    //    println!("{}", crate_char.to_string());
+    //}
 
-    return Some(vec!(vec!('a','b')));
+    // probably not the most efficient matrix mirroring in the world
+    // but this does prune the empties
+    let mut depot: Vec<Vec<char>> = Vec::new();
+    depot.push(Vec::new());
+    for stack in 0..scanlines[0].len() {
+        for row in scanlines.iter().rev() {
+            match row[stack] {
+                Some(x) => depot.last_mut().unwrap().push(x),
+                _ => {},
+            }
+        }
+        depot.push(Vec::new());
+    }
+
+    //println!("\nstack");
+    //for stacked_crate in &depot[1] {
+    //    println!("{}", stacked_crate.to_string());
+    //}
+
+    return Some(depot);
 }
 
 fn main() {
-    let mut foo = parse_crates(&mut stdin().lock()).unwrap();
+    let mut depot = parse_crates(&mut stdin().lock()).unwrap();
     //for line in stdin().lock().lines() {
     //}
     println!("Hello, world!");
