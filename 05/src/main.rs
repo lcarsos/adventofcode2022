180a62acb28863bd84f65ea80f9f741afd68e387
Ezekiel Chopper <lcarsos@lcarsos.com>
2022-12-23 10:31:37 -0700

    parsing scanlines

--- 05/src/main.rs
+++ 05/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{stdin, Read, BufRead, BufReader};
+use std::io::{stdin, Read, BufRead, BufReader, Take};
 use regex::Regex;
 
 fn parse_crates<R>(stream: &mut R) -> Option<Vec<Vec<char>>>
@@ -6,37 +6,43 @@ where
     R: BufRead,
 {
     let mut buffer = vec![0; 5];
-    let mut depot = vec![vec![b' '; 9]; 9];
-    let mut scanlines = vec![vec![b' '; 9; 9];
+    let mut scanlines: Vec<Vec<Option<u8>>> = Vec::new();
+    scanlines.push(Vec::new());
     let mut stack = 0;
     let mut row = 0;
+    let digit_re = Regex::new(r"^[0-9]$").unwrap();
 
-    let mut handle = stream.take(3);
-    handle.read(&mut buffer).unwrap();
-    match buffer [1] {
-        b' ' => {
-            stack += 1;
-            println!("found space");
-        },
-        x => {
-            scanlines[row][stack] = x
+    let mut handle;
+    loop {
+        handle = stream.take(3);
+        handle.read(&mut buffer).unwrap();
+        if buffer[0] == b' ' &&
+            digit_re.is_match(&(buffer[1] as char).to_string())
+        {
+            println!("breaking");
+            break;
         }
-    }
-    if buffer[1] == b' ' {
-    }
-    handle = stream.take(1);
-    handle.read(&mut buffer).unwrap();
-    println!("{}", buffer[0]);
-    match buffer[0] {
-        b'\n' => {
-            
-            println!("newline");
-        },
-        b' ' => println!("space"),
-        _ => panic!("parse could not match expected character"),
-    }
-    if buffer[0] == 10 {
-        
+        match buffer[1] {
+            b' ' => {
+                scanlines[row].push(None);
+                println!("found space");
+            },
+            x => {
+                println!("pushing {}", x as char);
+                scanlines[row].push(Some(x));
+            }
+        }
+        handle = stream.take(1);
+        handle.read(&mut buffer).unwrap();
+        println!("{}", buffer[0]);
+        match buffer[0] {
+            b'\n' => {
+                scanlines.push(Vec::new());
+                println!("newline");
+            },
+            b' ' => println!("space"),
+            _ => panic!("parse could not match expected character"),
+        }
     }
 
 
