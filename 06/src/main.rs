249f8bd0ac5ba1a37b6f7e048dc2ca73666d1e56
lcarsos <lcarsos@lcarsos.com>
2022-12-24 17:57:14 -0700

    mystifying. learned the difference between Copy and Clone.

--- 06/src/main.rs
+++ 06/src/main.rs
@@ -1,26 +1,23 @@
+use std::str;
 use std::io::{stdin, BufRead};
 use std::collections::HashSet;
 
-fn signal_lock_on(stream: &Vec<u8>) -> Option<usize> {
-    for sop in 3..stream.len() {
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
+}
+
+fn signal_lock_on(stream: &Vec<u8>, width: usize) -> Option<usize> {
+    for sop in width..stream.len() {
         // https://stackoverflow.com/questions/39803237/build-hashset-from-a-vector-in-rust
         // works on the whole array, but I just want a slice
         // sidenote: apparently stream[sop-3..sop] size is not known at compile time
-        let sequence = stream.get(sop-3..sop).unwrap();
-        //println!("test: {}", sequence.to_string());
-        let mut test = HashSet::<u8>::from_iter(sequence.cloned());
-        // error[E0599]: the method `cloned` exists for reference `&[u8]`, but its trait bounds were not satisfied
-        //   --> src/main.rs:10:58
-        //    |
-        // 10 |         let mut test = HashSet::<u8>::from_iter(sequence.cloned());
-        //    |                                                          ^^^^^^ method cannot be called on `&[u8]` due to unsatisfied trait bounds
-        //    |
-        //    = note: the following trait bounds were not satisfied:
-        //            `&[u8]: Iterator`
-        //            which is required by `&mut &[u8]: Iterator`
-        //            `[u8]: Iterator`
-        //            which is required by `&mut [u8]: Iterator`
-        if test.len() == 4 {
+        let sequence = stream.get(sop-width..sop).unwrap().iter().cloned();
+        {
+            //let debug_str = sequence.clone().collect::<Vec<u8>>();
+            //println!("test: {}", str::from_utf8(&sequence.clone().collect::<Vec<u8>>()).unwrap());
+        }
+        let mut test = HashSet::<u8>::from_iter(sequence);
+        if test.len() == width {
             return Some(sop);
         }
     }
@@ -33,6 +30,9 @@ fn main() {
         Ok(_) => {},
         Err(e) => panic!("panic: {e}"),
     }
-    let mut signal: Vec<u8> = Vec::from(in_string);
-    let target = signal_lock_on(&signal).unwrap();
+    let signal: Vec<u8> = Vec::from(in_string);
+    let target = signal_lock_on(&signal, 4).unwrap();
+    let message = signal_lock_on(&signal, 14).unwrap();
+    println!("{target}");
+    println!("{message}");
 }
