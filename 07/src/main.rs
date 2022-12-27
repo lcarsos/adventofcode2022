1c2636d1cb7fa283b83f0bf0e3887c68a00bce0f
lcarsos <lcarsos@lcarsos.com>
2022-12-27 13:40:41 -0700

    almost, but no banana

--- 07/src/main.rs
+++ 07/src/main.rs
@@ -1,3 +1,4 @@
+use std::rc::Rc;
 use std::io::{stdin, BufRead};
 use regex::Regex;
 
@@ -7,7 +8,8 @@ enum FileType {
 }
 
 struct INode {
-    parent: Option<Box<INode>>,
+    parent: Option<Rc<INode>>,
+    name: String,
     kind: FileType,
     size: usize,
     children: Vec<INode>,
@@ -17,18 +19,30 @@ fn parse_file_tree<T>(stream: &mut T) -> Option<Box<INode>>
 where
     T: BufRead,
 {
-    let command_regex: Regex = Regex::new(r"\$ ([[:alpha:]]+) ([[:alpha:]]+)")?;
+    let command_regex: Regex = Regex::new(r"\$ ([[:alpha:]]+) ([[:alpha:]]+)").ok()?;
 
-    let mut tree: Box<INode>;
-    let mut cursor: &mut INode;
+    let tree: Box<INode> = Box::new(INode {
+        parent: None,
+        name: "/".to_string(),
+        kind: FileType::Directory,
+        size: 0,
+        children: Vec::new(),
+    });
+    let mut cursor: Rc<INode>;
     for line_res in stream.lines() {
-        let line = line_res?;
+        let line = line_res.ok()?;
         if command_regex.is_match(&line) {
             let cap = command_regex.captures(&line)?;
-            match cap.get(1)?.text {
+            match cap.get(1)?.as_str() {
                 "cd" => {
-
+                    let directory = cap.get(2)?.as_str();
+                    match directory {
+                        "/" => cursor = Rc::new(*tree),
+                        ".." => cursor = Rc::clone(&cursor.parent?),
+                        dir => cursor = Rc::new(cursor.children.iter().find(|&x| x.name == dir ).unwrap()),
+                    }
                 },
+                _ => todo!(),
             }
         }
     }
