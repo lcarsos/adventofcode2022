055788b7f1b854e883ea7486608ff8c3fa58fdc2
Ezekiel Chopper <lcarsos@lcarsos.com>
2022-12-29 10:35:06 -0700

    moving parsing into their own functions

--- 07/src/main.rs
+++ 07/src/main.rs
@@ -27,7 +27,6 @@ fn parse_file_tree<T>(stream: &mut T) -> Option<RcINode>
 where
     T: BufRead,
 {
-    let command_regex: Regex = Regex::new(r"\$ ([[:alpha:]]+) ([[:alpha:]]+)").ok()?;
 
     let tree: RcINode = Rc::new(RefCell::new(INode {
         parent: None,
@@ -38,19 +37,13 @@ where
     }));
     let mut cursor: RcINode;
     for line_res in stream.lines() {
-        parse_command(Rc::clone(&tree), line_res
+        cursor = parse_command(Rc::clone(&tree), line_res);
         let line = line_res.ok()?;
         if command_regex.is_match(&line) {
             let cap = command_regex.captures(&line)?;
             match cap.get(1)?.as_str() {
                 "cd" => {
                     let directory = cap.get(2)?.as_str();
-                    match directory {
-                        "/" => cursor = Rc::clone(&tree),
-                        ".." => cursor = Rc::clone(&cursor.borrow().parent?),
-                        //dir => cursor = Rc::clone(&*cursor.borrow().children.iter().find(|&x| x.name == dir ).unwrap()),
-                        _ => todo!(),
-                    }
                 },
                 _ => todo!(),
             }
@@ -63,7 +56,32 @@ fn parse_command<T>(current: RcINode, stream: &mut T) -> RcINode
 where
     T: BufRead
 {
+    let command_regex: Regex = Regex::new(r"\$ ([[:alpha:]]+) ([[:alpha:]]+)").ok()?;
+    let line = stream.unwrap();
+    stream.next();
+
+    let cap = command_regex.captures(&line).unwrap();
+    if !command_regex.is_match(&line) {
+        panic!("not a command");
+    }
+    match cap.get(1).unwrap().as_str() {
+        "cd" => parse_change_dir(current, &cap.get(2)?.as_str(), &mut stream),
+        _ => todo!();
+    }
 }
+
+fn parse_change_dir<T>(current: RcINode, dir: &str, stream: &mut T) -> RcINode
+where
+    T: BufRead
+{
+    match dir {
+        "/" => Rc::clone(&current),
+        ".." => Rc::clone(&current.borrow().parent?),
+        //dir => cursor = Rc::clone(&*cursor.borrow().children.iter().find(|&x| x.name == dir ).unwrap()),
+        _ => todo!(),
+    }
+}
+
 
 
 
